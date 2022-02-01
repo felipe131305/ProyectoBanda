a856faace0cc5a2e797d9762f41a56b320282b2c
DANIELPACHECORINCON <danielpacherincon@gmail.com>
2022-02-01 16:56:04 -0500

    se cambiaron los estilos de los albumes

---
+++ assets/js/scriptAlbumes.js
@@ -0,0 +1,221 @@
+let albumes = [
+  {
+    titulo: "Appetite For Destruction | 1986 Sound City Session",
+    imagen: "../assets/img/albumes/imgAppetite1.webp",
+    info: "1986 • Álbum • 27 canciones",
+  },
+  {
+    titulo: "Appetite For Destruction (Super Deluxe)",
+    imagen: "../assets/img/albumes/imgAppetite2.webp",
+    info: "1986 • Álbum • 27 canciones",
+  },
+  {
+    titulo: "Street Of Dreams",
+    imagen: "../assets/img/albumes/imgStreet.webp",
+    info: "1986 • Álbum • 27 canciones",
+  },
+  {
+    titulo: "New Collection",
+    imagen: "../assets/img/albumes/imgNewCollection.jpg",
+    info: "1986 • Álbum • 27 canciones",
+  },
+  {
+    titulo: "Better",
+    imagen: "../assets/img/albumes/imgBetter.jpg",
+    info: "1986 • Álbum • 27 canciones",
+  },
+  {
+    titulo: "Chinese Democracy",
+    imagen: "../assets/img/albumes/imgChineseDemocracy.jpg",
+    info: "2008 • Álbum • 14 canciones",
+  },
+  {
+    titulo: "Greatest Hits",
+    imagen: "../assets/img/albumes/imgGreatest.jpg",
+    info: "2004 • Recopilacion • 15 canciones",
+  },
+  {
+    titulo: "Live Era: '87-'93",
+    imagen: "../assets/img/albumes/imgLiveEra.jpg",
+    info: "1999 • Álbum • 22 canciones",
+  },
+  {
+    titulo: "Use Your Illusion",
+    imagen: "../assets/img/albumes/imgUseYourIllusion.jpg",
+    info: "1998 • Recopilacion • 12 canciones",
+  },
+  {
+    titulo: "Sympathy For The Devil",
+    imagen: "../assets/img/albumes/imgSympathy.jpg",
+    info: "1994 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "Ain't It Fun",
+    imagen: "../assets/img/albumes/imgAin't.jpg",
+    info: "1993 • Sencillo • 3 canciones",
+  },
+  {
+    titulo: "Since I Don't Have You",
+    imagen: "../assets/img/albumes/imgSince.jpg",
+    info: "1993 • Sencillo/EP • 3 canciones",
+  },
+  {
+    titulo: "Estranged",
+    imagen: "../assets/img/albumes/imgEstranged.jpg",
+    info: "1993 • Sencillo/EP • 1 canciones",
+  },
+  {
+    titulo: "The Civil War EP",
+    imagen: "../assets/img/albumes/imgTheCivilWar.jpg",
+    info: "1993 • Sencillo/EP • 3 canciones",
+  },
+  {
+    titulo: "The Spaghetti Incident?",
+    imagen: "../assets/img/albumes/imgTheSpaghetti.jpg",
+    info: "1993 • Álbum • 13 canciones",
+  },
+  {
+    titulo: "Guns N' Roses - Knockin' On Heaven's Door",
+    imagen: "../assets/img/albumes/imgKnockin'.jpg",
+    info: "1992 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "Pretty Tied Up",
+    imagen: "../assets/img/albumes/imgrettyTied.jpg",
+    info: "1992 • Sencillo/EP • 1 canciones",
+  },
+  {
+    titulo: "Yesterdays",
+    imagen: "../assets/img/albumes/imgyesterday.jpg",
+    info: "1992 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "So Fine",
+    imagen: "../assets/img/albumes/imgSoFine.jpg",
+    info: "1992 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "14 Years",
+    imagen: "../assets/img/albumes/img14Years.jpg",
+    info: "1992 • Sencillo/EP • 1 canciones",
+  },
+  {
+    titulo: "Destruction, Lies: The Road To Illusion",
+    imagen: "../assets/img/albumes/imgDestructionLies.jpg",
+    info: "1992 • Álbum • 23 canciones",
+  },
+  {
+    titulo: "Don't Cry",
+    imagen: "../assets/img/albumes/imgDon'tCry.jpg",
+    info: "1991 • Sencillo/EP • 3 canciones",
+  },
+  {
+    titulo: "Live And Let Die",
+    imagen: "../assets/img/albumes/imgLIveAnd.jpg",
+    info: "1991 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "You Could Be Mine",
+    imagen: "../assets/img/albumes/imgYouCould.jpg",
+    info: "1991 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "November Rain",
+    imagen: "../assets/img/albumes/imgNovember.jpg",
+    info: "1991 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "Garden Of Eden",
+    imagen: "../assets/img/albumes/imgGardenOfEden.jpg",
+    info: "1991 • Sencillo/EP • 1 canciones",
+  },
+  {
+    titulo: "On Tour Now!",
+    imagen: "../assets/img/albumes/imgOnTourNow.jpg",
+    info: "1991 • Álbum • 11 canciones",
+  },
+  {
+    titulo: "Use Your Illusion (vol.1)",
+    imagen: "../assets/img/albumes/imgUseYourIllusion.jpg",
+    info: "1991 • Álbum • 16 canciones",
+  },
+  {
+    titulo: "Use Your Illusion (vol.2)",
+    imagen: "../assets/img/albumes/imgYourIllusion.jpg",
+    info: "1991 • Álbum • 14 canciones",
+  },
+  {
+    titulo: "Knockin' On Heavens Door",
+    imagen: "../assets/img/albumes/imgKnockin'2.jpg",
+    info: "1990 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "Appetite For Collection",
+    imagen: "../assets/img/albumes/imgAppetiteForCollection.jpg",
+    info: "1989 • Recopilación • 18 canciones",
+  },
+  {
+    titulo: "GN'R",
+    imagen: "../assets/img/albumes/imgGN'R.jpg",
+    info: "1989 • Sencillo/EP • 4 canciones",
+  },
+  {
+    titulo: "Patience",
+    imagen: "../assets/img/albumes/imgPatience.jpg",
+    info: "1989 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "Nightrain",
+    imagen: "../assets/img/albumes/imgNightrain.jpg",
+    info: "1989 • Sencillo/EP • 3 canciones",
+  },
+  {
+    titulo: "Paradise City",
+    imagen: "../assets/img/albumes/imgParadiceCity.jpg",
+    info: "1988 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "G N' R Lies",
+    imagen: "../assets/img/albumes/imgGN'RLies.jpg",
+    info: "1988 • Álbum • 8 canciones",
+  },
+  {
+    titulo: "Live From The Jungle",
+    imagen: "../assets/img/albumes/imgLiveFromJungle.jpg",
+    info: "1988 • Sencillo/EP • 6 canciones",
+  },
+  {
+    titulo: "Welcome To The Jungle",
+    imagen: "../assets/img/albumes/imgWelcomeJungle.jpg",
+    info: "1987 • Sencillo/EP • 2 canciones",
+  },
+  {
+    titulo: "It's So Easy · Mr. Brownstone",
+    imagen: "../assets/img/albumes/imgIt'sSoEasy.jpg",
+    info: "1987 • Sencillo/EP • 4 canciones",
+  },
+  {
+    titulo: "Live ?!★@ Like a Suicide",
+    imagen: "../assets/img/albumes/imgLiveLikeSuicide.jpg",
+    info: "1986 • Sencillo/EP • 4 canciones",
+  },
+];
+
+const crearCartas = (album) => {
+  let html = "";
+
+  album.forEach((item) => {
+    html += '<div class="col">';
+    html += '<div class="card h-100 mt-2 bg-secondary bg-opacity-50 ">';
+    html += `<img class="card-img-top p-3 pb-0  " src="${item.imagen}" alt="" >`;
+    html += '<div class="card-body">';
+    html += `<h5 class="card-title fw-bold text-white text-sm-center">${item.titulo}</h5>`;
+    html += `<p class="card-text text-sm-center"><small class="fw-bold text-white-50">${item.info}</small></p>`;
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+  });
+  document.getElementById("albumes").innerHTML = html;
+};
+
+crearCartas(albumes);
